a672cb44a49a812e424898547c0e788eb68c0bd6
marcusnogueira <marcus.nogueira@dnx.solutions>
2024-01-31 23:24:33 +1100

    Fixing assume role

--- network/_settings.tf
+++ network/_settings.tf
@@ -3,3 +3,11 @@ locals {
   workspace  = yamldecode(file("./workspaces/${terraform.workspace}.yaml"))
   aws_role   = "InfraDeployAccess"
 }
+
+provider "aws" {
+  region = local.workspace.aws_region
+
+  assume_role {
+    role_arn = "arn:aws:iam::${local.workspace.aws_account_id}:role/${local.aws_role}"
+  }
+}
